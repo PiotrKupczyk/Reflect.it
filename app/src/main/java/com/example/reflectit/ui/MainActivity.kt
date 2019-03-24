7d22ea44c55264cc92c28ea493f5e8c34560c4a7
brtkozak <brtkozak@gmail.com>
2019-03-24 12:30:59 +0100

    some layout changes

--- app/src/main/java/com/example/reflectit/ui/MainActivity.kt
+++ app/src/main/java/com/example/reflectit/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.reflectit.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -10,6 +11,8 @@ import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.reflectit.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.toolbar
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,12 +21,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar as Toolbar?)
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
 
         NavigationUI.setupActionBarWithNavController(this, navController)
+        supportActionBar!!.setTitle(getString(R.string.mainActivityTitle))
+
     }
 
 //    override fun onSupportNavigateUp(): Boolean {
