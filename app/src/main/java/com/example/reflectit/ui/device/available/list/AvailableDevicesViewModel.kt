7559564df78083ed3e44e0e2ebbc553da0891236
Piotr Kupczyk <piotr.kupczyk97@gmail.com>
2019-06-09 12:47:25 +0200

    Fixed pinview and changed colors and fixed merge conflicts

--- app/src/main/java/com/example/reflectit/ui/device/available/list/AvailableDevicesViewModel.kt
+++ app/src/main/java/com/example/reflectit/ui/device/available/list/AvailableDevicesViewModel.kt
@@ -24,8 +24,6 @@ class AvailableDevicesViewModel(private val manager: NsdManager) : ViewModel() {
 
     fun registerDiscoverService() : LiveData<ArrayList<Mirror>> {
         println("Discover registered")
-<<<<<<< HEAD
-
         NetworkService.registerService(manager, port = 5005)
         CoroutineScope(Dispatchers.IO).launch {
             NetworkService.discoverServices(manager) { inetAddress: InetAddress, port: Int ->
@@ -36,11 +34,6 @@ class AvailableDevicesViewModel(private val manager: NsdManager) : ViewModel() {
                 //TODO: call api to get mirror specific data such as GUID
 
             }
-=======
-        NetworkService.discoverServices(manager) { inetAddress: InetAddress, port: Int ->
-            availableDevices.appendAsync(Mirror(inetAddress as Inet4Address, port))
-            //TODO: call api to get mirror specific data such as GUID
->>>>>>> 81efffa3f8b015f99c2a6a2b91baf383fea42289
         }
         return availableDevices
     }
