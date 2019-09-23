e5b29f9d735b65973bba6e01afe2c44847a61a33
Dominik Słapa <dominik.slapa@gmail.com>
2019-09-23 21:33:12 +0200

    Fix displaying the same mirror only once

--- app/src/main/java/com/example/reflectit/ui/device/available/list/AvailableDevicesViewModel.kt
+++ app/src/main/java/com/example/reflectit/ui/device/available/list/AvailableDevicesViewModel.kt
@@ -24,10 +24,11 @@ class AvailableDevicesViewModel(private val manager: NsdManager) : ViewModel() {
         Log.d(TAG,"Finding available devices..")
         CoroutineScope(Dispatchers.IO).launch {
             NetworkService.discoverServices(manager) { inetAddress: InetAddress, port: Int ->
-                availableDevices.appendAsync(Mirror(inetAddress as Inet4Address, port))
                 if (inetAddress is Inet4Address) {
                     Log.d(TAG,"Found available device. Address: ${inetAddress}, port: ${port}")
-                    availableDevices.appendAsync(Mirror(inetAddress, port))
+                    if (!availableDevices.value!!.any { x -> x.ip == inetAddress }){
+                        availableDevices.appendAsync(Mirror(inetAddress, port))
+                    }
                 }
             }
         }
